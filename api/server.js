2d2611ae67d84cf0d4997dcc15664aa7d8412dce
oscar <caceres.oscar7@gmail.com>
2017-08-05 19:40:22 -0300

    add security for ddos

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ import logger from 'morgan';
 import config from 'config';
 import cors from 'cors'
 import Mongoose from 'mongoose';
+import helmet from 'helmet';
 
 Mongoose.Promise = global.Promise;
 Mongoose.connect(config.db_url,{useMongoClient: true});
@@ -19,7 +20,7 @@ app.use(body_parser.urlencoded({extended:true}));
 app.use(body_parser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));
-
+app.use(helmet());
 
 
 app.get("/", (req, res) => res.json({message: "Api Hotels Almundo"}));
--- server/index.js
+++ server/index.js
@@ -4,6 +4,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const logger = require('morgan')
+const helmet = require('helmet')
+const Ddos = require('ddos');
+const DDOSparams= {
+    maxcount: 30,
+    burst: 5,
+    limit: 20,
+    maxexpiry: 120,
+    checkinterval: 1,
+    errormessage: "Error",
+    testmode: false
+  }
+var ddos = new Ddos(DDOSparams);
 
 const app = express()
 // Express configuration
@@ -11,7 +23,8 @@ app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(helmet());
+app.use(ddos.express);
 // Fallback to index.html
 app.get('*', (req, res, next) => {
   if (req.accepts('html')) {
